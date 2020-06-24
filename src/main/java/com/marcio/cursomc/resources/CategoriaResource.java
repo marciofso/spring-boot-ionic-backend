567fb0af906ad8750856ca65ad27f818b62a4c1f
Marcio Fernandes <marciofso@hotmail.com>
2020-06-24 17:52:42 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/marcio/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/marcio/cursomc/resources/CategoriaResource.java
@@ -25,7 +25,6 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
-		
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
@@ -42,5 +41,11 @@ public class CategoriaResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
-
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}	
+	
 }
--- src/main/java/com/marcio/cursomc/services/CategoriaService.java
+++ src/main/java/com/marcio/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.marcio.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.marcio.cursomc.domain.Categoria;
 import com.marcio.cursomc.repositories.CategoriaRepository;
+import com.marcio.cursomc.services.exceptions.DataIntegrityException;
 import com.marcio.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,7 +23,7 @@ public class CategoriaService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
-	public Categoria insert(Categoria obj) {
+	public Categoria insert (Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
@@ -29,6 +31,16 @@ public class CategoriaService {
 	public Categoria update (Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete (Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos.");
+		}
 	}
 
 }
