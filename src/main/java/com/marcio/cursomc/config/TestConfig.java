86b621e415dac8d7d10b62a7cc0425c4fa8ae5b5
Marcio Fernandes <marciofso@hotmail.com>
2020-07-12 16:54:07 -0300

    MockEmailService com Logger. Padrões Strategy e Template

--- src/main/java/com/marcio/cursomc/config/TestConfig.java
+++ src/main/java/com/marcio/cursomc/config/TestConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.marcio.cursomc.services.DBService;
+import com.marcio.cursomc.services.EmailService;
+import com.marcio.cursomc.services.MockEmailService;
 
 @Configuration
 @Profile("test")
@@ -21,5 +23,11 @@ public class TestConfig {
 		dbService.instantiateTestDatabase();
 		return true;
 	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
+	}
+	
 
 }
---
+++ src/main/java/com/marcio/cursomc/services/EmailService.java
@@ -0,0 +1,14 @@
+package com.marcio.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.marcio.cursomc.domain.Pedido;
+
+public interface EmailService {
+	
+	void sendOrderConfirmationEmail (Pedido obj);
+	
+	void sendEmail(SimpleMailMessage msg);
+	
+
+}
